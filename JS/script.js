dc46e818a4237725598415c94e141c2da7d58b5b
naffrey <nick.caffrey@icloud.com>
2021-08-26 15:02:35 -0500

    README has project informaiton, HTML, CSS, JS files are created, basic pseudocode is in JS to layout everything needed to complete.

---
+++ JS/script.js
@@ -0,0 +1,60 @@
+// HOME
+    // user to be able to travel to create cards from button
+
+    // user to be able to travel to study cards that have preloaded examples
+
+    // user able to open modual to see "how to"
+
+// CREAT NEW CARDS :: Possible if using local storage but recommended to just creat a topic with preloaded cards.
+
+    // user is able to open how to modual to see how to utilize page
+
+    // have user able to type in text box for the front side of card
+
+    // User is able to type in text box for the back side of the card
+
+    // user is able to "submit" button
+
+    // front side of card is saved for application to access info
+
+    // back side of car is saved for application to access info
+
+    // user is able to travel to the study page.
+
+ // Study
+    // user is able to click button to view how to modual 
+
+    // user is able to view backside of flash card with button
+
+    // user is able to view backside with a keyboard stroke
+
+    // user is able to select correct button
+
+        // correct button hides card from showing up again  
+        
+        // correct button logs score
+
+        // correct button moves to next card
+
+    //  user is able to select inncorrect button
+        
+        // card is not removed from deck
+
+        // score updates to inncorrect
+
+        // moves onto the next card
+
+    // score updates to show how many are correct
+
+    // score updates to show how many are inncorrect
+
+    // user is able to know that they have correctly anwser the deck
+
+        // option to restart the deck
+
+        // option to be done.
+
+
+
+    
+    
